8112e5f937cdfdfefff4d8659f21f270cde672e6
niodio <adelson.inacio@sweetmedia.tech>
2022-01-25 00:57:28 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+// import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+// Vue.use(BootstrapVue);
+// Vue.use(IconsPlugin);
+createApp(App).use(store).use(router).mount("#app");
---
+++ src/views/Users.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="about container">
+    <h1>This is an users page</h1>
+<div class="">
+    <div class="card  " v-for="user in users">
+      <div class="card-header bg-dark text-light">
+        <h2>{{ user.name }}</h2>
+      </div>
+      <div class="card-body">
+        
+          <span v-for="post in user.posts">
+            <h4>{{ post.title }}</h4>
+            <p>{{ post.body }}</p>
+            <hr>
+          </span>
+         </div>
+      </div>
+    </div>
+  </div>
+
+ 
+</template>
+
+<script>
+  import axios from "axios";
+
+  export default {
+    data(){
+      return {
+        users: []
+      }
+    },
+    created() {
+      // fetch('http://localhost:8000/api/users')
+      //   .then(response => response.json())
+      //   .then(json => {
+      //     this.users = json;
+      //     console.log(json);
+      //   });
+
+        axios
+        .get('http://localhost:8000/api/users')
+        .then(response => {
+            this.users = response.data;
+          });
+    },
+  }
+</script>
